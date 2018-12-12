7203fc07f47e0ce9c75087b9de88b40ecf71eced
Deymaz <andrewpostzp@gmail.com>
2018-12-12 21:49:02 +0200

    added adaprive styles and window.onload for audio

--- js/audio.js
+++ js/audio.js
@@ -1,21 +1,23 @@
-let christmasSong = $('#audio');
-let audioOn = $('#audio_on');
-let audioOff = $('#audio_off');
-let isChrome = !!window.chrome && !!window.chrome.webstore;
+window.onload = function() {
+    let christmasSong = $('#audio');
+    let audioOn = $('#audio_on');
+    let audioOff = $('#audio_off');
+    let isChrome = !!window.chrome && !!window.chrome.webstore;
 
-function turnOn() {
-    audioOn.css('display', 'block');
-    audioOff.css('display', 'none');
-    christmasSong[0].play();
-}
+    function turnOn() {
+        audioOn.css('display', 'block');
+        audioOff.css('display', 'none');
+        christmasSong[0].play();
+    }
 
-function turnOff() {
-    audioOn.css('display', 'none');
-    audioOff.css('display', 'block');
-    christmasSong[0].pause();
-}
+    function turnOff() {
+        audioOn.css('display', 'none');
+        audioOff.css('display', 'block');
+        christmasSong[0].pause();
+    }
 
-isChrome && christmasSong[0].paused ? turnOff() : turnOn();
-$('svg').on('click', function() {
-    this.id === 'audio_on' ? turnOff() : turnOn()
-});
+    isChrome && christmasSong[0].paused ? turnOff() : turnOn();
+    $('svg').on('click', function () {
+        this.id === 'audio_on' ? turnOff() : turnOn()
+    });
+};
