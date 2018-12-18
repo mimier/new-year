e3eaa57d31947d24137c8645e2d9e969a0fd86c1
Deymaz <andrewpostzp@gmail.com>
2018-12-18 20:39:09 +0200

    version 1.1

--- js/audio.js
+++ js/audio.js
@@ -2,7 +2,6 @@ window.onload = function() {
     let christmasSong = $('#audio');
     let audioOn = $('#audio_on');
     let audioOff = $('#audio_off');
-    let isChrome = !!window.chrome && !!window.chrome.webstore;
 
     function turnOn() {
         audioOn.css('display', 'block');
@@ -16,7 +15,7 @@ window.onload = function() {
         christmasSong[0].pause();
     }
 
-    isChrome && christmasSong[0].paused ? turnOff() : turnOn();
+    !!window.chrome && christmasSong[0].paused ? turnOff() : turnOn();
     $('svg').on('click', function () {
         this.id === 'audio_on' ? turnOff() : turnOn()
     });
