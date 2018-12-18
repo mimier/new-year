a0e3274a3a22c4172172c4b821863111e8c52a13
Deymaz <andrewpostzp@gmail.com>
2018-12-18 21:32:53 +0200

    version 1.1.1

--- js/audio.js
+++ js/audio.js
@@ -1,22 +1,20 @@
-window.onload = function() {
-    let christmasSong = $('#audio');
-    let audioOn = $('#audio_on');
-    let audioOff = $('#audio_off');
+let christmasSong = $('#audio');
+let audioOn = $('#audio_on');
+let audioOff = $('#audio_off');
 
-    function turnOn() {
-        audioOn.css('display', 'block');
-        audioOff.css('display', 'none');
-        christmasSong[0].play();
-    }
+function turnOn() {
+    audioOn.css('display', 'block');
+    audioOff.css('display', 'none');
+    christmasSong[0].play();
+}
 
-    function turnOff() {
-        audioOn.css('display', 'none');
-        audioOff.css('display', 'block');
-        christmasSong[0].pause();
-    }
+function turnOff() {
+    audioOn.css('display', 'none');
+    audioOff.css('display', 'block');
+    christmasSong[0].pause();
+}
 
-    !!window.chrome && christmasSong[0].paused ? turnOff() : turnOn();
-    $('svg').on('click', function () {
-        this.id === 'audio_on' ? turnOff() : turnOn()
-    });
-};
+!!window.chrome ? turnOff() : turnOn();
+$('svg').on('click', function () {
+    this.id === 'audio_on' ? turnOff() : turnOn()
+});
